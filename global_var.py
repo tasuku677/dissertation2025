db3e0d7c60357f6e6c34e8279de2a43c40a921e1
tasuku677 <78686862+tasuku677@users.noreply.github.com>
2025-09-10 03:54:16 +0900

    Add readme

--- global_var.py
+++ global_var.py
@@ -4,7 +4,7 @@ class SimConfig:
     SIM_DURATION = 40  # seconds
     TIME_STEP = 1  # 1秒ごとにシミュレーションを更新
     VELOCITY_RANGE = (8.9, 31.3)  # 20-70 mphをm/sに変換
-    COMM_RANGE = 250  # 通信範囲 (m) - 仮定 論文では50-300m 
+    COMM_RANGE = 300  # 通信範囲 (m) - 仮定 論文では50-300m 
     INITIAL_TRUST = 0.5
     INITIAL_ENERGY = 26.25 * 3600  # Whをジュールに変換 (仮)
     ENERGY_TX = 0.00000046 * 1e-9 # nJ/bitをJ/bitに変換
